897b07c988db82cb3968d2bc652bee19dfb96318
Nodaco35 <ngodoancuong699668@gmail.com>
2025-08-24 15:14:06 +0700

    Feature: Add new Goals and validate Goals

--- habit-tracker/src/goals/Goal.jsx
+++ habit-tracker/src/goals/Goal.jsx
@@ -27,16 +27,26 @@ export default function Goal() {
 
     const [startDate, setStartDate] = useState();
     const [newGoal, setNewGoal] = useState({
+        userId: user ? user.id : null,
         name: "",
         description: "",
         startDate: "",
         deadline: "",
         priority: "medium",
         status: "in_progress",
+        targetValue: 1,
         currentValue: 0,
-        unit:"",
+        unit: "",
         linkedHabits: []
-    })
+    });
+    const [error, setError] = useState({
+        name: "",
+        description: "",
+        startDate: "",
+        deadline: "",
+        linkedHabits: "",
+        unit: ""
+    });
 
 
     //FETCH DATA
@@ -98,6 +108,122 @@ export default function Goal() {
             fetchFilteredGoals();
         }
     }, [filters])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let valid = true;
+        if (newGoal.name.trim() == "") {
+            setError(prev => ({ ...prev, name: "Goal name is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, name: "" }));
+            valid = true;
+        }
+        if (newGoal.startDate == "") {
+            setError(prev => ({ ...prev, startDate: "Start date is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, startDate: "" }));
+            valid = true;
+        }
+        if (newGoal.deadline == "") {
+            setError(prev => ({ ...prev, deadline: "Deadline is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, deadline: "" }));
+            valid = true;
+        }
+        if (newGoal.linkedHabits.length == 0) {
+            setError(prev => ({ ...prev, linkedHabits: "At least one habit must be linked" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, linkedHabits: "" }));
+            valid = true;
+        }
+        if (newGoal.description.trim() == "") {
+            setError(prev => ({ ...prev, description: "Description is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, description: "" }));
+            valid = true;
+        }
+        if (newGoal.targetValue <= 0) {
+            setError(prev => ({ ...prev, targetValue: "Target value must be greater than 0" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, targetValue: "" }));
+            valid = true;
+        }
+        if (newGoal.unit.trim() == "") {
+            setError(prev => ({ ...prev, unit: "Unit is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, unit: "" }));
+            valid = true;
+        }
+        if (!newGoal.startDate) {
+            setError(prev => ({ ...prev, startDate: "Start date is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, startDate: "" }));
+            valid = true;
+        }
+        if (!newGoal.deadline) {
+            setError(prev => ({ ...prev, deadline: "Deadline is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, deadline: "" }));
+            valid = true;
+        }
+        if(newGoal.unit.trim() == ""){
+            setError(prev => ({ ...prev, unit: "Unit is required" }));
+            valid = false;
+        }
+        else {
+            setError(prev => ({ ...prev, unit: "" }));
+            valid = true;
+        }
+
+        if (!valid) return;
+        else {
+            const res = await fetch('http://localhost:8080/goals', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newGoal)
+            });
+            if (res.ok) {
+                const data = await res.json();
+                console.log(data);
+                handleClose();
+                fetchGoals();
+                fetchHabits();
+                setNewGoal({
+                    userId: user ? user.id : null,
+                    name: "",
+                    description: "",
+                    startDate: "",
+                    deadline: "",
+                    priority: "medium",
+                    status: "in_progress",
+                    targetValue: 0,
+                    currentValue: 0,
+                    unit: "",
+                    linkedHabits: []
+                });
+            }
+        }
+    }
 
     return (
         <div className="container py-4">
@@ -218,28 +344,79 @@ export default function Goal() {
                 </Modal.Header>
                 <Modal.Body>
                     <form>
-                        <label className="d-block mb-2">Goal Name</label>
-                        <input type="text" className="form-control mb-3" onChange={(e)=>setNewGoal({...newGoal, name: e.target.value})}/>
+                        <div className="d-flex gap-3">
+                            <div className="flex-3">
+                                <label className="d-block mb-2">Goal Title</label>
+                                <input type="text" placeholder="e.g., Run 100km This Month" className={`form-control mb-3 ${error.name ? "is-invalid" : ""}`} onChange={(e) => {
+                                    setNewGoal({ ...newGoal, name: e.target.value });
+                                    if (e.target.value && e.target.value.trim() !== "") {
+                                        setError(prev => ({ ...prev, name: "" }));
+                                        console.log(e.target.value);
+                                    }
+                                }} />
+                                {error.name && <p className="text-red-500 text-sm mb-2">{error.name}</p>}
+                            </div>
+                            <div className="flex-1">
+                                <label className="d-block mb-2">Priority</label>
+                                <Form.Select
+                                    value={newGoal.priority}
+                                    onChange={(e) =>
+                                        setNewGoal({ ...newGoal, priority: e.target.value })
+                                    }
+                                >
+                                    <option value="medium">Medium</option>
+                                    <option value="high">High</option>
+                                </Form.Select>
+                            </div>
+                        </div>
 
                         <label className="d-block mb-2">Description</label>
-                        <textarea className="form-control" rows="3" onChange={(e)=>setNewGoal({...newGoal, description: e.target.value})}></textarea>
+                        <textarea className={`form-control ${error.description ? "is-invalid" : ""}`} placeholder="Describe your goal in detail..." rows="3" onChange={(e) => {
+                            setNewGoal({ ...newGoal, description: e.target.value });
+                            if (e.target.value && e.target.value.trim() !== "") {
+                                setError(prev => ({ ...prev, description: "" }));
+                            }
+                        }}></textarea>
+                        {error.description && <p className="text-red-500 text-sm mb-2">{error.description}</p>}
 
-                        <label className="d-block mb-2 mt-3">Start date</label>
-                        <input type="date" className="form-control mb-3" min={new Date().toISOString().split("T")[0]} onChange={(e)=>setNewGoal({...newGoal, startDate: e.target.value})} />
+                        <div className="d-flex gap-2">
+                            <div className="flex-1">
+                                <label className="d-block mb-2 mt-3">Start date</label>
+                                <input type="date" className={`form-control mb-3 ${error.startDate ? "is-invalid" : ""}`} min={new Date().toISOString().split("T")[0]} onChange={(e) => {
+                                    setNewGoal({ ...newGoal, startDate: e.target.value })
+                                    if (e.target.value) {
+                                        setError(prev => ({ ...prev, startDate: "" }));
+                                    }
+                                }} />
+                                {error.startDate && <p className="text-red-500 text-sm mb-2">{error.startDate}</p>}
+                            </div>
 
-                        <label className="d-block mb-2">Deadline</label>
-                        <input type="date" className="form-control mb-3" min={newGoal.startDate} disabled={!newGoal.startDate} onChange={(e)=>setNewGoal({...newGoal, deadline: e.target.value})}/>
+                            <div className="flex-1">
+                                <label className="d-block mb-2 mt-3">Deadline</label>
+                                <input type="date" className="form-control mb-3" min={newGoal.startDate} disabled={!newGoal.startDate} onChange={(e) => setNewGoal({ ...newGoal, deadline: e.target.value })} />
+                                {error.deadline && <p className="text-red-500 text-sm mb-2">{error.deadline}</p>}
+                            </div>
 
-                        <label className="d-block mb-2">Priority</label>
-                        <Form.Select
-                                value={newGoal.priority}
-                                onChange={(e) =>
-                                    setNewGoal({ ...newGoal, priority: e.target.value })
-                                }
-                            >
-                                <option value="medium">Medium</option>
-                                <option value="high">High</option>
-                            </Form.Select>
+                        </div>
+
+                        <div className="d-flex gap-2">
+
+                            <div className="flex-1">
+                                <label className="d-block mb-2 mt-3">Unit</label>
+                                <input type="text" className={`form-control mb-3 ${error.unit ? "is-invalid" : ""}`} placeholder="e.g., km, books, days" onChange={(e) => {
+                                    setNewGoal({ ...newGoal, unit: e.target.value })
+                                    if (e.target.value && e.target.value.trim() !== "") {
+                                        setError(prev => ({ ...prev, unit: "" }));
+                                    }
+                                }} />
+                                {error.unit && <p className="text-red-500 text-sm mb-2">{error.unit}</p>}
+                            </div>
+                            <div className="flex-1">
+                                <label className="d-block mb-2 mt-3">Target Value</label>
+                                <input type="number" className={`form-control mb-3}`} min={1} onChange={(e) => setNewGoal({ ...newGoal, targetValue: e.target.value })} value={1} />
+                                {error.targetValue && <p className="text-red-500 text-sm mb-2">{error.targetValue}</p>}
+                            </div>
+                        </div>
 
                         <label className="d-block mb-2">Link Habit</label>
                         {habits?.map((habit) => (
@@ -250,6 +427,7 @@ export default function Goal() {
                                 </label>
                             </div>
                         ))}
+                        {error.linkedHabits && <p className="text-red-500 text-sm mb-2">{error.linkedHabits}</p>}
 
                     </form>
                 </Modal.Body>
@@ -257,7 +435,7 @@ export default function Goal() {
                     <Button variant="secondary" onClick={handleClose}>
                         Hủy
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={(e) => handleSubmit(e)}>
                         Lưu
                     </Button>
                 </Modal.Footer>
